40253db3e627c44906cf68c2a2f47fa050fbe4b9
Anna <anna.viklund1@medieinstitutet.se>
2025-06-10 17:00:09 +0200

    Tried to add animated clouds, fail

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,31 @@
 import { motion } from "framer-motion";
+import clouds from "../assets/clouds.png";
+import "../styles/pages/_home.scss";
 
 export const Home = () => {
   return (
     <>
       <div className="home">
-        <h1>Welcome to the Zooo</h1>
-        <p>Meet our pixel friends</p>
-        <motion.button
-          className="button"
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.95, rotate: "-2deg" }}
-          transition={{ type: "spring", stiffness: 300 }}
-          onClick={() => alert("Animal is fed!")}
-        >
-          Feed me
-        </motion.button>
+        <div className="sky">
+          <motion.img
+            src={clouds}
+            alt="clouds"
+            className="cloud cloud1"
+            animate={{ x: ["-300px", "100vw"] }}
+            transition={{ duration: 45, repeat: Infinity, ease: "linear" }}
+          />
+          <motion.img
+            src={clouds}
+            alt="clouds"
+            className="cloud cloud2"
+            animate={{ x: ["100vw", "-300px"] }}
+            transition={{ duration: 60, repeat: Infinity, ease: "linear" }}
+          />
+        </div>
+        <div className="content">
+          <h1>Welcome to the Zooo</h1>
+          <p>Meet our pixel friends</p>
+        </div>
       </div>
     </>
   );
