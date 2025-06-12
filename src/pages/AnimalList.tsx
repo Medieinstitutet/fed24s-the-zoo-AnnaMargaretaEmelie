075663748b392aafa815d47ebde04b1303fc1aa9
Anna <anna.viklund1@medieinstitutet.se>
2025-06-12 13:31:37 +0200

    Added custom hook for fetch

--- src/pages/AnimalList.tsx
+++ src/pages/AnimalList.tsx
@@ -1,9 +1,23 @@
+import { useFetch } from "../hooks/useFetch";
+import type { IAnimal } from "../models/Animal";
+
 export const AnimalList = () => {
+  const {
+    loading,
+    data: animals,
+    error,
+  } = useFetch<IAnimal[]>("https://animals.azurewebsites.net/api/animals");
+  if (loading) return <p>Loading those animals</p>;
+  if (error) return <p>Something went wrong: {error}</p>;
   return (
     <>
       <div className="animal-list">
-        <h2>All animals</h2>
-        <p>(Animal data)</p>
+        {animals?.map((animal) => (
+          <div key={animal.id}>
+            <h3>{animal.name}</h3>
+            <p>{animal.shortDescription}</p>
+          </div>
+        ))}
       </div>
     </>
   );
