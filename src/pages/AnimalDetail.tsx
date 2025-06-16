1679738021664a1937ef19e3721581516668f8df
Anna <anna.viklund1@medieinstitutet.se>
2025-06-16 13:13:31 +0200

    Added page animaldetail. Fixed problem in model so that longDescription now is visible

--- src/pages/AnimalDetail.tsx
+++ src/pages/AnimalDetail.tsx
@@ -1,11 +1,30 @@
 import { useParams } from "react-router";
+import { useFetch } from "../hooks/useFetch";
+import type { IAnimal } from "../models/Animal";
 
 export const AnimalDetail = () => {
   const { id } = useParams();
+  const {
+    loading,
+    data: animal,
+    error,
+  } = useFetch<IAnimal>(`https://animals.azurewebsites.net/api/animals/${id}`);
+
+  if (loading) return <p>Laddar ditt valda djur.</p>;
+  if (error || !animal) return <p>Kunde inte hämta djuret.</p>;
   return (
     <>
-      <div>
-        <h2>Animal no: {id}</h2>
+      <div className="animal-detail">
+        <h2>{animal.name}</h2>
+        <img
+          src={animal.imageUrl}
+          alt={animal.name}
+          onError={(e) => (e.currentTarget.src = "/fallback.png")}
+        />
+        <p>
+          {animal.longDescription ||
+            "Ingen ytterligare information är tillgänglig."}
+        </p>
       </div>
     </>
   );
