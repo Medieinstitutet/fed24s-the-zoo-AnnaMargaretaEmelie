0528871cdd8f38e8af0f9a49069268560af9f27e
Anna <anna.viklund1@medieinstitutet.se>
2025-06-17 10:58:12 +0200

    Added enums and reducer

---
+++ src/reducers/animalReducer.ts
@@ -0,0 +1,23 @@
+import type { IAnimal } from "../models/Animal";
+import  { AnimalActionTypes } from "./AnimalActionTypes";
+
+export type AnimalAction =  {
+    type: AnimalActionTypes; 
+    payload: IAnimal[] | number;}
+
+export const animalReducer = (state: IAnimal[], action: AnimalAction):IAnimal[] =>{
+    switch (action.type) {
+        case AnimalActionTypes.SET: {
+            const animals = action.payload as IAnimal[]
+            return animals; }
+
+        case AnimalActionTypes.FED: {
+        const id = action.payload as number;
+        const now = new Date().toISOString();
+        localStorage.setItem(`fed-${id}`, now);
+        return state.map((a)=> a.id === id ? { ...a,lastFed: now }: a
+    );
+}
+        default: return state;
+    }
+}
